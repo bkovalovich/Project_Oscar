b93226e17b1b11f0eab9562dc0816034e479426a
bkova <dampnoodle123@gmail.com>
2025-08-12 00:24:17 -0400

    initial commit

---
+++ Project_Oscar/Assets/Scripts/WaterManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+    [SerializeField] Transform start, end;
+    [SerializeField] int points; 
+    private LineRenderer line; 
+    private void Awake() {
+        line = GetComponent<LineRenderer>();
+        CreatePoints();
+    }
+    private void CreatePoints() {
+        line.positionCount = points;
+        Vector3 distanceBetween = start.position - end.position;
+        Debug.Log($"between: {distanceBetween}");
+        float step = Mathf.Abs(distanceBetween.x) / (line.positionCount - 1);
+        Debug.Log(step);
+
+        for (int i = 0; i < line.positionCount; i++) {
+            line.SetPosition(i, new Vector3(start.position.x + (i * step), start.position.y, 0));
+        }
+    }
+}
